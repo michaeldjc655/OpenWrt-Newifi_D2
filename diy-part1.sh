cd74d5cc3f16c7f5da6e6716fab53d8f4a730add
michaeldjc655 <50612302+michaeldjc655@users.noreply.github.com>
2021-01-05 09:22:53 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,6 +9,11 @@
 #=============================================================
 
 # fw876/helloworld
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-#sed -i 's@coolsnowwolf/packages@P3TERX/packages@' feeds.conf.default
+# Add a feed source
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git michael655 https://github.com/michael6555/openwrt-packages' feeds.conf.default
+# sed -i '$a src-git kenzok8 https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+# sed -i '$a src-git sirpdboypackage https://github.com/siropboy/sirpdboy-package' feeds.conf.default
